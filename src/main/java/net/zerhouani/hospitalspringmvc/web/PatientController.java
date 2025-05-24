5a628aac32f2cdfaaca564e5d5c79f672c0d07f7
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-24 13:25:54 +0100

    2.2

--- src/main/java/net/zerhouani/hospitalspringmvc/web/PatientController.java
+++ src/main/java/net/zerhouani/hospitalspringmvc/web/PatientController.java
@@ -3,9 +3,12 @@ package net.zerhouani.hospitalspringmvc.web;
 import lombok.AllArgsConstructor;
 import net.zerhouani.hospitalspringmvc.entities.Patient;
 import net.zerhouani.hospitalspringmvc.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,9 +17,11 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("ListePatients",patientList);
+    public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page,
+                        @RequestParam(name="size", defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("pagePatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 }
