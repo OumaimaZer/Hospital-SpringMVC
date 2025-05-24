25a336ebe575ca736a6236034d45b7f9507b56b7
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-24 09:37:53 +0100

    Initialization

---
+++ src/main/java/net/zerhouani/hospitalspringmvc/HospitalSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package net.zerhouani.hospitalspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/zerhouani/hospitalspringmvc/HospitalSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zerhouani.hospitalspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
