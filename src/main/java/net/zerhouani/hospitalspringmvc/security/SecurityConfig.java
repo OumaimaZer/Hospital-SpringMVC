fde9771c8c0456c90b7e48ee7dfb920ba1cc5538
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-26 13:39:58 +0100

    Third part: Security 2

--- src/main/java/net/zerhouani/hospitalspringmvc/security/SecurityConfig.java
+++ src/main/java/net/zerhouani/hospitalspringmvc/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package net.zerhouani.hospitalspringmvc.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,9 +19,9 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password("{noop}123").roles("USER").build(),
-                User.withUsername("user2").password("{noop}123").roles("USER").build(),
-                User.withUsername("admin").password("{noop}123").roles("USER", "ADMIN").build()
+                User.withUsername("user1").password(passwordEncoder().encode("123")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("123")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("123")).roles("USER", "ADMIN").build()
         );
     }
 
@@ -29,11 +32,15 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/login")         // Optional: custom login page
-                        .defaultSuccessUrl("/home")  // Where to go after successful login
-                        .permitAll()                 // Allow everyone to access the login page
+                        .defaultSuccessUrl("/index")
+                        .permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
